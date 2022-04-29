e4ebb108074e6a3dae4c37a9c61366ccb9b89765
AzureAD\YlliNeziri <ylli.neziri@covar.io>
2022-04-29 11:29:59 +0200

    AppBar added

---
+++ src/components/ui/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {AppBar, Toolbar, useScrollTrigger} from "@material-ui/core";
+
+function ElevationScroll(props) {
+    const { children } = props;
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+    });
+
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    });
+}
+
+export default function Header(props){
+    return (
+        <ElevationScroll>
+            <AppBar position='fixed'>
+                <Toolbar>My first react app</Toolbar>
+            </AppBar>
+        </ElevationScroll>
+    );
+}
