fb36566e88b78b5952a11ccab88a94ee607bfb9d
AzureAD\YlliNeziri <ylli.neziri@covar.io>
2022-04-29 11:49:25 +0200

    theme-provider added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "./components/ui/Header";
+import { ThemeProvider } from '@material-ui/styles'
+import theme from './components/ui/Theme';
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
         <Header/>
         Hi!
-    </div>
+    </ThemeProvider>
   );
 }
 
