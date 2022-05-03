412ad2b10485e86473a9d690ce9f5ea1e6250692
Ylli Neziri <ylli.neziri@covar.io>
2022-05-03 09:25:19 +0200

    React routing + refresh page doesn't set the selected tab to home due to useEffect + button styled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { ThemeProvider } from "@material-ui/styles";
 import Header from "./components/ui/Header";
+import {lightTheme} from "./components/ui/Theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-        <Header/>
-        Hi!
-    </div>
+    <ThemeProvider theme={lightTheme}>
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path="/" component={() =>
+                    <div>
+
+                 </div>}/>
+                <Route path="/services" component={() => <div>services</div>}/>
+                <Route path="/customsoftware" component={() => <div>custom software</div>}/>
+                <Route path="/mobileapps" component={() => <div>mobile apps</div>}/>
+                <Route path="/websites" component={() => <div>websites</div>}/>
+                <Route path="/revolution" component={() => <div>revolution</div>}/>
+                <Route path="/about " component={() => <div>about</div>}/>
+                <Route path="/contact" component={() => <div>contact</div>}/>
+                <Route path="/estimate " component={() => <div>estimate</div>}/>
+            </Routes>
+        </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import {AppBar, ThemeProvider, useScrollTrigger, Toolbar, Tab, Tabs, makeStyles} from "@material-ui/core";
+import React, {useEffect, useState} from 'react';
+import {AppBar, Button, makeStyles, Tab, Tabs, ThemeProvider, Toolbar, useScrollTrigger,} from "@material-ui/core";
 import {lightTheme} from "./Theme";
+import {Link} from 'react-router-dom';
+
 import logo from '../../assets/logo.svg'
 
 function ElevationScroll(props) {
-    const { children } = props;
+    const {children} = props;
     const trigger = useScrollTrigger({
         disableHysteresis: true,
         threshold: 0,
@@ -15,14 +17,21 @@ function ElevationScroll(props) {
     });
 }
 
-const useStyles = makeStyles(theme => {
-    return ({
+
+const useStyles = makeStyles(theme => ({
+
         toolbarMargin: {
             ...theme.mixins.toolbar,
             marginBottom: "3em"
         },
         logo: {
-            height: "7em"
+            height: "8em"
+        },
+        logoContainer: {
+          padding: 0,
+          "&:hover": {
+              backgroundColor: "transparent"
+          }
         },
         tabContainer: {
             marginLeft: 'auto'
@@ -32,29 +41,65 @@ const useStyles = makeStyles(theme => {
             minWidth: 10,
             marginLeft: "25px",
         },
-    });
-})
+        button: {
+            ...lightTheme.typography.estimate,
+            borderRadius: "50px",
+            marginLeft: "50px",
+            marginRight: "25px",
+            height: "45px",
+        }
+    })
+);
 
-export default function Header(props){
+export default function Header(props) {
     const classes = useStyles();
+    const [value, setValue] = useState(0);
+
+    const handleChange = (e, value) => {
+        setValue(value)
+    }
+
+    useEffect(() => {
+        if (window.location.pathname === "/" && value !== 0) {
+            setValue(0);
+        } else if (window.location.pathname === "/services" && value !== 1) {
+            setValue(1);
+        } else if (window.location.pathname === "/revolution" && value !== 2) {
+            setValue(2);
+        } else if (window.location.pathname === "/about" && value !== 3) {
+            setValue(3);
+        } else if (window.location.pathname === "/contact" && value !== 4) {
+            setValue(4);
+        } else if (window.location.pathname === "/estimate" && value !== 5){
+            setValue(5);
+        }
+    }, [value]);
 
     return (
         <ThemeProvider theme={lightTheme}>
             <ElevationScroll>
-                <AppBar position='fixed' color="primary">
+                <AppBar position='fixed'>
                     <Toolbar disableGutters>
-                        <img alt= "company logo" src={logo} className={classes.logo}/>
-                        <Tabs className={classes.tabContainer}>
-                            <Tab className={classes.tab} label="Home"/>
-                            <Tab className={classes.tab} label="Services"/>
-                            <Tab className={classes.tab} label="The Revolution"/>
-                            <Tab className={classes.tab} label="About us"/>
-                            <Tab className={classes.tab} label="Contact us"/>
+                        <Button component={Link} to="/" className={classes.logoContainer} onClick={handleChange} disableRipple>
+                        <img alt="company logo" src={logo} className={classes.logo}/>
+                        </Button>
+                        <Tabs
+                            value={value}
+                            onChange={handleChange}
+                            className={classes.tabContainer}
+                            indicatorColor="primary">
+                            <Tab className={classes.tab} component={Link} to="/" label="Home"/>
+                            <Tab className={classes.tab} component={Link} to="/services" label="Services"/>
+                            <Tab className={classes.tab} component={Link} to="/revolution" label="The Revolution"/>
+                            <Tab className={classes.tab} component={Link} to="/about" label="About us"/>
+                            <Tab className={classes.tab} component={Link} to="/contact" label="Contact us"/>
                         </Tabs>
+                        <Button variant="contained" color="secondary" className={classes.button}>
+                            Free Estimate
+                        </Button>
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
-            <div className={classes.toolbarMargin}/>
         </ThemeProvider>
     );
 }
