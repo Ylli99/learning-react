eda4bcb73bb2769a79c26713f203b2b55833a1df
Ylli Neziri <ylli.neziri@covar.io>
2022-05-24 14:05:59 +0200

    no message

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,25 @@
 const functions = require('firebase-functions');
+const config = functions.config();
+const nodemailer = require("nodemailer")
+const admin = require("firebase-admin");
+admin.initializeApp()
 
+const transporter = nodemailer.createTransport({
+    service: "Gmail", auth: {user: config.user.email, pass: config.user.password}
+});
+
+let mailOptions = {
+    from: 'Arc Development', to: "ylliineziri@gmail.com", subject: 'Testing nodemailer', text: 'Test successful'
+}
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.sendMail = functions.https.onRequest((request, response) => {
+    transporter.sendMail(mailOptions, error => {
+        if (error) {
+            response.send(erro)
+        } else {
+            response.send("Message sent successfully")
+        }
+    })
+});
